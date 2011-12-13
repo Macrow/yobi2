78f574ca6aeec3f6a0837f10e36014a7afef4d61
Macrow <Macrow_wh@163.com>
2011-12-13 16:46:46 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yobi2::Application.config.secret_token = '4a6b7905413fb4d0999cae032764be580364be7ddb2774c4f5067a783892df2fbd35c22a450a7dff6be948c365fbd7417e39cc16acb62eea211dbc355a55a2d7'
